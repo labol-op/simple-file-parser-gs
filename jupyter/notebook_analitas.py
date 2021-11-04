39095097b7ba67827cc28b42f7c1b48ef33dddab
losmariio <j.mariio1991@gmail.com>
2021-11-04 17:06:18 -0400

    finalizando script do relatório

--- jupyter/notebook_analitas.py
+++ jupyter/notebook_analitas.py
@@ -1,47 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[79]:
+# In[177]:
 
 
 import os
 import pandas as pd
 import gspread
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
-# In[116]:
+# In[187]:
 
 
 def formatar_data(data) -> str:
-    data_formatada = datetime.strftime(data, format = "%d/%m/%Y")
+    DATA_INICIAL = datetime(1899, 12, 30)
+    diferenca_datas = data - DATA_INICIAL
+    data_formatada = diferenca_datas.days
     return data_formatada
 
 def formatar_hora(hora) -> str:
-    hora_formatada = hora.strftime(format = "%H:%M:%S")
+    params = {
+    'hours': hora.hour,
+    'minutes': hora.minute,
+    'seconds': hora.second}
+    
+    hora_formatada = timedelta(**params) / timedelta(days=1)
     return hora_formatada
 
 
-# In[15]:
+# In[153]:
+
+
+def limpar_intervalo(aba,intervalo):
+    
+    aba.batch_clear([intervalo])
+    
+
+
+# In[154]:
 
 
 x = pd.read_excel('order_h.XLSX')
 
 
-# In[17]:
+# In[155]:
 
 
 _bopisloja =  x[['Centro', 'Pedido Internet - Bemol On-line', 'Data do Pedido Bemol On-line']]
 
 
-# In[85]:
+# In[191]:
 
 
 bopisloja = _bopisloja.query('Centro > 0')
 bopisloja['Data do Pedido Bemol On-line'] = bopisloja['Data do Pedido Bemol On-line'].apply(formatar_data)
 
 
-# In[126]:
+# In[192]:
 
 
 bopishora = x[['Pedido Internet - Bemol On-line','Data do Pedido Bemol On-line',
@@ -51,21 +67,21 @@ bopishora['Hora do Pedido Bemol On-line'] = bopishora['Hora do Pedido Bemol On-l
 bopishora.fillna('', inplace = True)
 
 
-# In[41]:
+# In[193]:
 
 
 basepedidos = x[['Pedido Internet - Bemol On-line','Data do Pedido Bemol On-line',
                'Hora do Pedido Bemol On-line','Códido de Identificação do site']]
 
 
-# In[130]:
+# In[194]:
 
 
 basepedidos['Data do Pedido Bemol On-line'] = basepedidos['Data do Pedido Bemol On-line'].apply(formatar_data)
 basepedidos['Hora do Pedido Bemol On-line'] = basepedidos['Hora do Pedido Bemol On-line'].apply(formatar_hora)
 
 
-# In[47]:
+# In[195]:
 
 
 fname = os.path.abspath("./test-auto-330418-221ec3d586fa.json")
@@ -73,49 +89,83 @@ gc = gspread.service_account(fname)
 sh = gc.open("Cópia de Acompanhamento de pedidos SETEMBRO 2021")
 
 
-# In[49]:
+# In[196]:
 
 
 sh_bopisloja = sh.worksheet('BOPIS Recebido Loja')
 
 
-# In[50]:
+# In[197]:
 
 
 #sh_bopisloja.acell('B3').value
 
 
-# In[137]:
+# In[201]:
 
 
 bopisloja_num = bopisloja.shape[0]+4
+limpar_intervalo(sh_bopisloja,'A4:C1500')
 sh_bopisloja.update(f'A4:C{bopisloja_num}',bopisloja.values.tolist())
+sh_bopisloja.format(f'C2:C{bopisloja_num}',
+                    {'numberFormat':
+                     {'type':
+                      "DATE"
+                      }
+                    })
 
 
-# In[96]:
+# In[202]:
 
 
 sh_bopishora = sh.worksheet('BOPIS por Hora')
 
 
-# In[125]:
+# In[203]:
 
 
 bopishora_num = bopishora.shape[0]+2
+limpar_intervalo(sh_bopishora,'A2:E1500')
+
 sh_bopishora.update(f'A2:E{bopishora_num}',bopishora.values.tolist())
+sh_bopishora.format(f'C2:C{bopishora_num}',
+                    {'numberFormat':
+                     {'type':
+                      "TIME"
+                      }
+                    })
+sh_bopishora.format(f'B2:B{bopishora_num}',
+                    {'numberFormat':
+                     {'type':
+                      "DATE"
+                      }
+                    })
 
 
-# In[128]:
+# In[204]:
 
 
 sh_basepedidos =  sh.worksheet('Base PEDIDOS h/h')
 
 
-# In[132]:
+# In[206]:
 
 
 basepedidos_num = basepedidos.shape[0]+2
+limpar_intervalo(sh_basepedidos,'A2:D2500')
 sh_basepedidos.update(f'A2:D{basepedidos_num}',basepedidos.values.tolist())
+sh_basepedidos.format(f'C2:C{basepedidos_num}',
+                    {'numberFormat':
+                     {'type':
+                      "TIME"
+                      }
+                    })
+sh_basepedidos.format(f'B2:B{basepedidos_num}',
+                    {'numberFormat':
+                     {'type':
+                      "DATE"
+                      }
+                    })
 
 
 # In[ ]:
